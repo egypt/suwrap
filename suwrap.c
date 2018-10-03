7a4b0f1321f18a7c40f52914fb4c86849121e3a7
James Lee <egypt@metasploit.com>
2018-10-03 17:19:15 -0500

    Use select instead of forking

--- suwrap.c
+++ suwrap.c
@@ -1,3 +1,5 @@
+// Cribbed heavily from Scriptjunkie's stdiobindshell.c
+
 #include <errno.h>
 #include <termios.h>
 #include <stdio.h>
@@ -7,6 +9,7 @@
 #include <pty.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/select.h>
 #include <signal.h>
 
 #define BUF_SIZE 255
@@ -61,7 +64,7 @@ int main(int argc, char * const *argv)
     // grab the password from the user
     fgets(buf, BUF_SIZE, stdin);
     //buf[strcspn(buf, "\n")] = 0;
-    buf[strlen(buf)-1] = 0;
+    //buf[strlen(buf)-1] = 0;
 
     // forward it to su
     write(terminalfd, buf, strlen(buf));
@@ -77,61 +80,96 @@ int main(int argc, char * const *argv)
         exit(1);
     }
 
-    // Fork to go into forwarding loops for input and output would probably be
-    // cleaner with non-blocking IO in one thread, but this made sense to me
-    // and I'm lazy
-    pid_t forkpid = fork();
-    if(forkpid == 0)
-    {
-        while(1) // loop while reading, logging, and writing to pty user's input
-        {
-            int numbytes = read(0, buf, BUF_SIZE - 1); //read from stdin
-            if (numbytes == -1) // on error, if we're really done, then kill ourselves, otherwise continue
-            {
-                if(errno == 5)
-                {
-                    break;
-                }
-                continue;
-            }
-            write(terminalfd, buf, numbytes);
-        }
-    }
-    else
-    {
-        pid_t childpid = forkpid;
-        tcgetattr(0, &old); // Disable buffered I/O and echo mode for terminal emulated
-        new = old;
-        new.c_lflag &= ~ICANON;
-        new.c_lflag &= ~ECHO;
-        tcsetattr(0, TCSANOW, &new);
+    tcgetattr(0, &old); // Disable buffered I/O and echo mode for terminal emulated
+    new = old;
+    new.c_lflag &= ~ICANON;
+    new.c_lflag &= ~ECHO;
+    tcsetattr(0, TCSANOW, &new);
 
-        while (1) // loop while reading and echoing the pty's output
+    fd_set rfds;
+    FD_ZERO(&rfds);
+    while (1)
+    {
+        int status;
+        int numbytes;
+
+        struct timeval tv;
+        int retval;
+
+        /* Watch stdin (fd 0) to see when it has input. */
+
+        FD_SET(0, &rfds);
+        FD_SET(terminalfd, &rfds);
+
+        /* Wait up to five seconds. */
+
+        tv.tv_sec = 5;
+        tv.tv_usec = 0;
+
+        retval = select(FD_SETSIZE, &rfds, NULL, NULL, &tv);
+
+        if (retval == -1)
         {
-            int status;
-            int numbytes = read(terminalfd, buf, BUF_SIZE - 1);
+            perror("select()");
+        }
+        else if (retval)
+        {
+            //printf("Data is available now. %d\n", retval);
+            /* FD_ISSET(0, &rfds) will be true. */
+        }
+        else
+        {
+            printf("No data within five seconds.\n");
+            continue;
+        }
+
+        // I don't like this big chunk of duplicate code, but I don't see an
+        // easy way around it
+        if (FD_ISSET(0, &rfds))
+        {
+            printf("Data is available from stdin. %d\n", retval);
+            numbytes = read(0, buf, BUF_SIZE - 1);
+
             if (numbytes == -1) // on error, if we're really done, then kill ourselves, otherwise continue
             {
-                printf("[wrap]read -1, %d\n", errno);
                 if(errno == 5)
                 {
                     waitpid(pid, &status, WNOHANG);
-                    printf("[wrap]status: %d\n", status);
-                    kill(childpid, SIGTERM);
                     tcsetattr(0, TCSANOW, &old); //reset terminal
                     if (WIFEXITED(status))
                     {
-                        printf("[wrap]exited\n");
                         exit(WEXITSTATUS(status));
                     } else {
-                        printf("[wrap]didn't exit\n");
                         exit(1);
                     }
                 }
-                continue;
+            }
+            write(terminalfd, buf, numbytes);
+        }
+
+
+        if (FD_ISSET(terminalfd, &rfds))
+        {
+            numbytes = read(terminalfd, buf, BUF_SIZE - 1);
+            printf(" %d bytes\n", numbytes);
+
+            if (numbytes == -1) // on error, if we're really done, then kill ourselves, otherwise continue
+            {
+                if(errno == 5)
+                {
+                    waitpid(pid, &status, WNOHANG);
+                    tcsetattr(0, TCSANOW, &old); //reset terminal
+                    if (WIFEXITED(status))
+                    {
+                        exit(WEXITSTATUS(status));
+                    } else {
+                        exit(1);
+                    }
+                }
             }
             write(1, buf, numbytes);
         }
+
     }
 
     exit(EXIT_SUCCESS);
